e309e8ee3d4ebcb2258fc1189f3760a1682f5f2a
Olgamay <olga48915042@yandex.ru>
2021-07-19 20:06:51 +0300

    555

--- src/pages/ListPage/ListPage.js
+++ src/pages/ListPage/ListPage.js
@@ -1,22 +1,44 @@
 import React, { Component } from 'react';
 import './ListPage.css';
 
+const apikey = '46383995';
+
 class ListPage extends Component {
     state = {
         movies: [
             { title: 'The Godfather', year: 1972, imdbID: 'tt0068646' }
-        ]
+        ],
+        imdbID: ""
     }
     componentDidMount() {
-        const id = this.props.match.params;
-        console.log(id);
+        let id = this.props.match.params.id;
+        console.log('https://acb-api.algoritmika.org/api/movies/list/' +id);
+        console.log(id)
+
+        console.log(` http://www.omdbapi.com/?s=${this.state.searchLine}&apikey=${apikey}`);
+        console.log(id)
+
         // TODO: запрос к сервер на получение списка
-        
+        fetch('https://acb-api.algoritmika.org/api/movies/list/' +id)
+            .then(response => response.json())
+            .then(data => {
+                fetch('https://acb-api.algoritmika.org/api/movies/list/' +imdbID)
+                    .then(response => response.json())
+                    .then(data => {
+                        console.log("imdbID", data);
+                    })
+            // фетч запрос к imdbID и цикл по шв
 
-
-
-        
+            // фетч запрос к imdbID и цикл по шв
+                console.log("id", data);
+            })
+            .catch(error => {
+                console.log("Произошла ошибка");
+            });
+       
         // TODO: запросы к серверу по всем imdbID
+
+
     }
     render() { 
         return (
@@ -26,7 +48,7 @@ class ListPage extends Component {
                     {this.state.movies.map((item) => {
                         return (
                             <li key={item.imdbID}>
-                                <a href="https://www.imdb.com/title/tt0068646/" target="_blank">{item.title} ({item.year})</a>
+                                <a href="#" target="_blank">{item.title} ({item.year})</a>
                             </li>
                         );
                     })}

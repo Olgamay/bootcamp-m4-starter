7a5b3c074e62ec37eaee95c868fe0efa2013a1c1
Olgamay <olga48915042@yandex.ru>
2021-07-20 21:06:43 +0300

    22+

--- src/pages/ListPage/ListPage.js
+++ src/pages/ListPage/ListPage.js
@@ -5,6 +5,7 @@ import Favorites from "../../components/Favorites/Favorites";
 let result = [];
 let imdbID = [];
 let apikey = '1b6eef56';
+// Почему не активен apikey, он подставляется внизу в ссылке ключ актуальный. 
 
 
 
@@ -26,20 +27,8 @@ class ListPage extends Component {
         // TODO: запрос к сервер на получение списка
         fetch('https://acb-api.algoritmika.org/api/movies/list/' +id)
             .then(response => response.json())
-            .then((data) => {
-                // console.log("636", data.movies)
-                // let listMovies = [...this.state.movies];
-                // listMovies.push(data.movies)
-                // console.log("111", listMovies)
-                // let list = listMovies
-
-                
-                // for(let i = 0; i < list.length; i++) {
-                //     imdbID.push(list[i].imdbID)
-                //     console.log("44", imdbID)
-                // }
-                
-                fetch('http://www.omdbapi.com/?i=${this.state.searchLine}&apikey=${apikey}')
+            .then((data) => {                
+                fetch('http://www.omdbapi.com/?i=${this.state.props.searchLine}&apikey=${apikey}')
                     .then(response => response.json())
                     .then(data => {
                         imdbID = {...data}
@@ -47,6 +36,11 @@ class ListPage extends Component {
                         this.setState({movies: result})
                         console.log("77", data)
                 }) 
+                // this.state.searchLine это ссылка Favorites поэтому пропс
+
+
+
+
             // // фетч запрос к imdbID и цикл по шв
 
             // // фетч запрос к imdbID и цикл по шв
